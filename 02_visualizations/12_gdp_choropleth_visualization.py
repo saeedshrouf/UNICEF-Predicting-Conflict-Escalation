eec59cdf6c49101f3c4611a4e3158d2ceda750c5
Saeed Al-Shrouf <96080630+saeedshrouf@users.noreply.github.com>
2024-06-04 13:37:20 +0400

    Update 12_gdp_choropleth_visualization.py

--- 02_visualizations/12_gdp_choropleth_visualization.py
+++ 02_visualizations/12_gdp_choropleth_visualization.py
@@ -1,10 +1,6 @@
-df = tp.merge(cid, left_on='iso3', right_on='iso3', how='inner')
-fig = []
-for model, colors in zip(['y_pred_proba_transformer', 'y_pred_proba_ffnn', 'y_pred_proba_xgboost'], ['Reds', 'Blues', 'Greens']):
-    fig += [go.Figure(data=go.Choropleth(
-        locations=df['iso3'], text=df['iso3'], z=df[model],
-        colorscale=colors, autocolorscale=False, reversescale=False,
-        marker_line_width=0.5, colorbar_tickprefix='', colorbar_title=model))]
-fig[0].show()
-fig[1].show()
-fig[2].show()
+pio.renderers.default = 'notebook'
+progress_indicator = 'sowc_social-protection-and-equity__gdp-per-capita-current-us-2010-2019-r_bottom-2-value'
+go.Figure(data=go.Choropleth(
+    locations=cid['iso3'], text=cid['iso3'], z=cid[progress_indicator],
+    colorscale='Greens', autocolorscale=False, reversescale=False,
+    marker_line_width=0.5, colorbar_tickprefix='', colorbar_title="GDP per capita"))
